75b6b86c2068a8f60515357e1c4dc65655947121
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-03 16:37:06 +0000

    Updates v1 module to use correct paths

--- projects/backend/src/v1/v1.module.ts
+++ projects/backend/src/v1/v1.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { HealthcheckModule } from './healthcheck/healthcheck.module';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from './user/user.module';
-import { SpaceModule } from './spaces/space.module';
+import { SpaceModule } from './space/space.module';
 import { LocationModule } from './location/location.module';
 import { BoxModule } from './box/box.module';
 import { ItemModule } from './item/item.module';
