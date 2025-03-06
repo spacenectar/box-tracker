a6a8b4c34666a649e5c3c3c9863cebf59463860b
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-06 22:03:43 +0000

    Moves user checking from app/app/page to app/app/layout

--- projects/webapp/app/app/layout.tsx
+++ projects/webapp/app/app/layout.tsx
@@ -5,12 +5,15 @@ import { useDispatch } from 'react-redux'
 import { useRouter } from 'next/navigation'
 import { setToken } from '@/lib/store/auth-slice'
 import Masthead from "@components/layout/masthead"
+import { useGetUserQuery } from '@/lib/services/user'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const { getToken, isSignedIn, isLoaded } = useAuth()
   const dispatch = useDispatch()
   const router = useRouter()
   const [isTokenFetched, setIsTokenFetched] = useState(false)
+
+  const { data, error, isLoading, refetch } = useGetUserQuery();
 
   useEffect(() => {
     async function fetchToken() {
@@ -22,7 +25,16 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       setIsTokenFetched(true)
     }
     fetchToken()
+
   }, [getToken, dispatch, isSignedIn])
+
+
+  useEffect(() => {
+    if (isTokenFetched && isSignedIn && !data) {
+      refetch()
+    }
+  }, [isTokenFetched, isSignedIn, data, refetch])
+
 
   // Show a loader while Clerk is still determining authentication status
   if (!isLoaded) {
@@ -30,7 +42,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   }
 
   // Once Clerk is loaded, but the user isn't signed in, show access denied message
-  if (!isSignedIn) {
+  if (!isSignedIn && isLoaded && !isLoading && !isTokenFetched && !data) {
     return (
       <div className='dashboard-layout ta-c flex flex-column gap-2 items-center'>
         <h1 className='heading-large'>Access Denied</h1>
@@ -40,13 +52,24 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     )
   }
 
-  if (!isTokenFetched) {
+  if (!isTokenFetched || isLoading || !data) {
     return <div className='dashboard-layout'>Loading...</div>
+  }
+
+  if (error && !isLoaded) {
+    return <div className='dashboard-layout ta-c flex flex-column gap-2 items-center'>
+      <h1 className='heading-large'>Error</h1>
+      <p>There was an error fetching your user data.</p>
+      <button className='btn-primary' onClick={() => router.refresh()}>Try Again</button>
+      <p>
+        If the problem persists, please try again later.
+      </p>
+    </div>
   }
 
   return (
     <>
-      <Masthead />
+      <Masthead user={data!}/>
       {children}
     </>
   )
--- projects/webapp/app/app/page.tsx
+++ projects/webapp/app/app/page.tsx
@@ -1,24 +1,11 @@
 'use client'
 
-import { useGetUserQuery } from "@/lib/services";
-
 export default function Home() {
-
-  const { data, error, isLoading } = useGetUserQuery();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error instanceof Error ? error.message : 'An unknown error occurred'}</div>;
-  }
-
   return (
     <div className="dashboard-layout">
       <main className="ta-c">
 
-        <p>You are logged in as {data?.username}</p>
+        <p>Sup!</p>
 
       </main>
     </div>
