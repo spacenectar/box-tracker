3ea3ee42bdd1f54da0fdf3cb5ceae1a11d8b96a1
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-06 18:32:14 +0000

    Adds new mocks library

---
+++ libs/mocks/users.ts
@@ -0,0 +1,40 @@
+export const users = [
+  {
+
+    name: 'Jean Valjean',
+    email: 'm.madeleine@montreuil.gov.fr',
+    username: '24601',
+    password: '24601',
+    id: 'fd21d3e3-27a1-405f-90cd-583c701f85b2'
+  },
+  {
+    name: 'Javert',
+    email: 'javert@hotmail.com',
+    username: 'countingstars',
+    password: 'iamtheLAW',
+    id: 'fd21d3e3-27a1-405f-90cd-583c701f85b3'
+  },
+  {
+    name: 'Fantine',
+    email: 'hair4sale@gmail.com',
+    username: 'lovelylady',
+    password: 'ilovecosette',
+    id: 'fd21d3e3-27a1-405f-90cd-583c701f85b4'
+  },
+  {
+    name: 'Cosette',
+    email: 'c.fauchelevent@icloud.com',
+    username: 'castleonacloud',
+    password: 'marius4ever',
+    id: 'fd21d3e3-27a1-405f-90cd-583c701f85b5'
+  },
+  {
+    name: 'Marius',
+    email: 'marius@abc-society.org',
+    username: 'vivalarevolution',
+    password: 'cosette4ever',
+    id: 'fd21d3e3-27a1-405f-90cd-583c701f85b6'
+  }
+];
+
+export default users;
