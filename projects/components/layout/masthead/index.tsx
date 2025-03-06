f7996ab587beaec8eb3f6c0099083db18aeb3059
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-06 18:57:07 +0000

    WIP: Working on styling the masthead

--- projects/components/layout/masthead/index.tsx
+++ projects/components/layout/masthead/index.tsx
@@ -2,37 +2,49 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './styles.module.scss';
+import { User } from '@types/user';
 
 import Logo from '@assets/branding/box-tracker-logo-white.svg'
 
-export const Masthead: React.FC = () => {
+export interface Props extends React.HTMLAttributes<HTMLElement> {
+  user: User
+}
+
+const navItems = [
+  {
+    label: 'Home',
+    href: '/'
+  },
+  {
+    label: 'About',
+    href: '/about'
+  },
+  {
+    label: 'Contact',
+    href: '/contact'
+  }
+];
+
+export const Masthead: React.FC<Props> = ({ user, ...props }: Props) => {
   return (
-    <header className={styles.masthead}>
-      <div className={styles.logo}>
-        <Link href="/">
-          <h1>
+    <header className={styles.masthead} {...props}>
+        <h1>
+          <Link href="/" className={styles.logo}>
             <Image src={Logo} alt="Box Tracker Logo" width={50} height={50} />
-            <span className='title'>Box Tracker</span>
-          </h1>
-        </Link>
-      </div>
+            <span className={styles.title}>Box Tracker</span>
+          </Link>
+        </h1>
       <nav className={styles.nav}>
         <ul>
-          <li>
-            <Link href="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact">
-              Contact
-            </Link>
-          </li>
+          {navItems.map((item) => (
+            <li key={item.label}>
+              <Link
+              className={styles['nav-item']}
+              href={item.href }>
+                {item.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
