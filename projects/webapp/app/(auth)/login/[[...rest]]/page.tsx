a46e98e47b496e5ffc0cc8158fe520317fccaa9c
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-11 17:44:30 +0000

    fixup! Route Grouping I realised I needed to still have a different set of layouts for unauthenticated routes (login, logout, register etc...), so rather than restore the /app folder I created route groups for auth (not a great name is it's actually unauthenticated but that felt clunky and I couldn't think of a better name, they are all related to authentication so I think its fine) and 'main' for the actual app itself.

--- projects/webapp/app/(auth)/login/[[...rest]]/page.tsx
+++ projects/webapp/app/(auth)/login/[[...rest]]/page.tsx
@@ -28,6 +28,7 @@ export default function LoginPage() {
         }
       }}
       fallbackRedirectUrl="/"
+      signUpUrl="/register"
       />
     </div>
   );
--- projects/webapp/app/(auth)/register/[[...rest]]/page.tsx
+++ projects/webapp/app/(auth)/register/[[...rest]]/page.tsx
@@ -30,6 +30,7 @@ export default function RegisterPage() {
           }
         }}
         fallbackRedirectUrl="/"
+        signInUrl="/login"
       />
     </div>
   );
