49b0c68c4124f072de6f263dd3b62144bfd15734
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-11 18:42:41 +0000

    Updates the loader to be more accessible as it now uses aria tags and has screenreader context

--- libs/components/feedback/loader/index.tsx
+++ libs/components/feedback/loader/index.tsx
@@ -5,30 +5,33 @@ import styles from './styles.module.scss'
 
 interface Props {
   /**
-   * Text to display below the loader
-   * only visible to screen readers
+   * Text to display for screen readers while loading
    */
   helpText?: string
-  className?: string,
+  className?: string
 }
 
 /**
- * A loading component that displays a bouncing square loader
- * @param param0 
- * @returns 
+ * A loading component that displays a bouncing square loader.
+ * Screen readers will be notified of the loading state via an ARIA live region.
  */
-export const Loader: React.FC<Props> = ({ className = '', helpText='Loading...' }) => {
+export const Loader: React.FC<Props> = ({
+  className = '',
+  helpText = 'Loading...',
+}) => {
   return (
-    <>
-      <div className={`${styles.loader} ${className}`}>
-        <div className={`${styles.square} ${styles.square1}`}></div>
-        <div className={`${styles.square} ${styles.square2}`}></div>
-        <div className={`${styles.square} ${styles.square3}`}></div>
-        <div className={`${styles.square} ${styles.square4}`}></div>
-        <div className={`${styles.square} ${styles.square5}`}></div>
-      </div>
+    <div
+      role="status"
+      aria-live="polite"
+      className={`${styles.loader} ${className}`}
+    >
+      <div className={`${styles.square} ${styles.square1}`} />
+      <div className={`${styles.square} ${styles.square2}`} />
+      <div className={`${styles.square} ${styles.square3}`} />
+      <div className={`${styles.square} ${styles.square4}`} />
+      <div className={`${styles.square} ${styles.square5}`} />
       <span className="visually-hidden">{helpText}</span>
-    </>
+    </div>
   )
 }
 
